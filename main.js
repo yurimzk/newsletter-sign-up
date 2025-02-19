32c8fcc1eb254c7365e9b6798b508dfd5bd083aa
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-02-19 17:45:53 -0300

    add form logic and refactor the code

--- main.js
+++ main.js
@@ -4,24 +4,33 @@ const successPage = document.getElementById('success-page');
 const email = document.getElementById('email');
 const buttonDismiss = document.getElementById('button-dismiss');
 const emailError = document.getElementById('email-error');
-const emailInput = document.getElementById('email');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
+function handleSubmit(e) {
+  e.preventDefault();
 
-  successPage.classList.remove('hidden');
-  app.classList.add('hidden');
-})
+  const data = email.value;
+  const isValidEmail = /^\S+@\S+$/g;
+
+  if (!isValidEmail.test(data)) {
+    emailError.classList.add('error-message');
+    emailError.style.display = 'block';
+    email.classList.add('error-input');
+    email.style.display = 'block';
+  } else {
+    successPage.classList.remove('hidden');
+    app.classList.add('hidden');
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
 
 buttonDismiss.addEventListener('click', (event) => {
   event.preventDefault();
 
   successPage.classList.add('hidden');
   app.classList.remove('hidden');
+  email.value = '';
+  emailError.style.display = 'none';
+  emailError.classList.remove('error-message');
+  email.classList.remove('error-input');
 });
-
-emailError.classList.add('error-message');
-emailError.style.display = 'block';
-
-emailInput.classList.add('error-input');
-emailInput.style.display = 'block';
