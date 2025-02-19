d044ffa2701e31fc91df02a792db89253d6dde5b
Yuri Tagomori <goncalvessyuri@gmail.com>
2025-02-19 16:15:57 -0300

    add logic to style error input

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const successPage = document.getElementById('success-page');
 const email = document.getElementById('email');
 const buttonDismiss = document.getElementById('button-dismiss');
 const emailError = document.getElementById('email-error');
+const emailInput = document.getElementById('email');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -19,4 +20,8 @@ buttonDismiss.addEventListener('click', (event) => {
   app.classList.remove('hidden');
 });
 
-  // emailError.classList.add('error-message');
+emailError.classList.add('error-message');
+emailError.style.display = 'block';
+
+emailInput.classList.add('error-input');
+emailInput.style.display = 'block';
